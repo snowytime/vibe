ada0f73031fc35ee312f87e1b5be4dd237959647
Snaer <87044139+snowytime@users.noreply.github.com>
2022-12-25 14:26:56 -0700

    🥳 auto patch

--- src/node/cli/dev.ts
+++ src/node/cli/dev.ts
@@ -1,10 +1,13 @@
 import { GenericError } from "@errors/index.js";
-import { getVibeData } from "../index.js";
+import { findFolder, findStories, getConfig, getVibeData } from "../index.js";
 
 export const dev = async () => {
 	try {
-		const data = await getVibeData();
-		console.log(data);
+		const [folder] = await findFolder();
+		const config = await getConfig(folder);
+		const stories = await findStories(config);
+		// const data = await getVibeData();
+		console.log({ config, stories });
 		// const [folder] = await findFolder();
 		// const config = await getConfig(folder);
 		// const { local, network } = await config.addon.dev();
