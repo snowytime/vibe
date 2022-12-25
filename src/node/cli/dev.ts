df5f33fed5f59617f22ea0fae74cba2f4acebd69
Snaer <87044139+snowytime@users.noreply.github.com>
2022-12-25 15:28:45 -0700

    🥳 auto patch

--- src/node/cli/dev.ts
+++ src/node/cli/dev.ts
@@ -1,26 +1,14 @@
 import { GenericError } from "@errors/index.js";
-import {
-	findFolder,
-	findStories,
-	getConfig,
-	getStoryData,
-	getVibeData
-} from "../index.js";
+import { main_logger } from "@logs/index.js";
+import { findFolder, getConfig } from "../index.js";
 
 export const dev = async () => {
 	try {
 		const [folder] = await findFolder();
 		const config = await getConfig(folder);
-		const stories = await findStories(config);
-		const storyData = await getStoryData(stories);
-		// const data = await getVibeData();
-		console.log({ config, stories, storyData });
-		// const [folder] = await findFolder();
-		// const config = await getConfig(folder);
-		// const { local, network } = await config.addon.dev();
-		// main_logger({ local, network });
+		const { local, network } = await config.addon.dev();
+		main_logger({ local, network });
 	} catch (e) {
-		console.log(e);
 		throw new GenericError({ message: "Unable to parse for stories" });
 	}
 };
