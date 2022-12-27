ae64d1fd42345a32c7c6d281527f8b943f712b16
Snaer <87044139+snowytime@users.noreply.github.com>
2022-12-27 14:51:54 -0700

    🥳 auto patch

--- src/ast/storyname-meta.ts
+++ src/ast/storyname-meta.ts
@@ -1,44 +1,42 @@
-import { VisbyDefaultExport } from "./default.js";
-
-export const extractStorynameMeta = (
-	namedData: any[],
-	ast: any[],
-	family: VisbyDefaultExport
-) => {
+export const extractStorynameMeta = (namedData, ast, family) => {
 	const assignments = ast.filter(
 		(i) => i?.expression?.type === "AssignmentExpression"
 	);
-	assignments.forEach((assignment) => {
-		// we need to find the component name
-		if (assignment?.expression?.left.property?.value === "storyName") {
-			const component = namedData.find(
-				(entry) =>
-					entry.componentName ===
-					assignment.expression.left.object.value
-			);
-			component["storyName"] = assignment.expression.right.value;
+	namedData.forEach((named) => {
+		let meta = {};
+		if (family.meta) {
+			meta = family.meta;
 		}
-		// possible meta
-		if (assignment?.expression?.left?.property?.value === "meta") {
-			const meta = {
-				...(family.meta ? family.meta : {})
-			};
-			getMeta(assignment.expression.right.properties, meta);
+		named["meta"] = meta;
+	});
+	assignments.forEach((assignment) => {
+		if (assignment.expression.left.object?.value) {
+			const isStoryName =
+				assignment.expression.left.property.value === "storyName";
+			const isMeta = assignment.expression.left.property.value === "meta";
+			if (!isStoryName && !isMeta) {
+				return;
+			}
 			const component = namedData.find(
 				(entry) =>
 					entry.componentName ===
-					assignment.expression.left.object.value
+					assignment.expression.left.object?.value
 			);
-			component["meta"] = meta;
+			if (isStoryName) {
+				component["storyName"] = assignment.expression.right.value;
+			}
+			if (isMeta) {
+				const customMeta = {};
+				getMeta(assignment.expression.right.properties, customMeta);
+				component["meta"] = { ...component.meta, ...customMeta };
+			}
 		}
 	});
 };
-
-const getMeta = (property: any, obj: VisbyDefaultExport) => {
-	property.forEach((prop: any) => getMetaEntry(prop, obj));
+const getMeta = (property, obj) => {
+	property.forEach((prop) => getMetaEntry(prop, obj));
 };
-
-const getMetaEntry = (property: any, meta: any) => {
+const getMetaEntry = (property, meta) => {
 	if (property.value.type.includes("Literal")) {
 		meta[property.key.value] = property.value.value;
 	}
@@ -46,9 +44,8 @@ const getMetaEntry = (property: any, meta: any) => {
 		meta[property.key.value] = extractArray(property);
 	}
 };
-
-const extractArray = (property: any) => {
-	const arr = property.value.elements.map((element: any) => {
+const extractArray = (property) => {
+	const arr = property.value.elements.map((element) => {
 		return element.expression.value;
 	});
 	return arr;
