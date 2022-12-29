bdb8ea9ef588882d1e7aa864345a5db6306fb58d
Snaer <87044139+snowytime@users.noreply.github.com>
2022-12-29 14:41:05 -0700

    🥳 auto patch

--- src/main/index.ts
+++ src/main/index.ts
@@ -3,13 +3,19 @@
  * will be available to you
  */
 import { getConfig } from "@config/index.js";
-import { findConfig, findFolder, findStories } from "@finders/index.js";
+import {
+	findConfig,
+	findFolder,
+	findStories,
+	findVite
+} from "@finders/index.js";
 import { getStoryData } from "@parsers/index.js";
 import { generateTree, getJson } from "@structures/index.js";
 
 export const getVibeData = async () => {
 	const [folderPath] = await findFolder();
 	const [configPath] = await findConfig(folderPath);
+	const [vitePath] = await findVite(folderPath);
 	const config = await getConfig(folderPath);
 	const stories = await findStories(config);
 	const storyData = await getStoryData(stories);
@@ -20,6 +26,7 @@ export const getVibeData = async () => {
 		stories: storyData,
 		json,
 		storyTree,
-		subscribers: [...stories, configPath]
+		subscribers: [...stories, configPath],
+		vitePath
 	};
 };
