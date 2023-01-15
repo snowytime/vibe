c4734cfdad2a1d2f160907cb130289c77e7a3646
Snaer <87044139+snowytime@users.noreply.github.com>
2023-01-14 22:41:17 -0700

    🥳 auto patch

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -36,6 +36,7 @@ const Main = () => {
                             />
                         );
                     })}
+                    <Route path='*' element={<Navigate to='/' replace />} />
                 </Routes>
             </Context.Provider>
         </BrowserRouter>
