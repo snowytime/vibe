1ea0db2f1ad6263e5bef91de4f2f73f553da2d9c
Snaer <87044139+snowytime@users.noreply.github.com>
2023-01-15 16:52:52 -0700

    🥳 auto patch

--- src/plugin/entry.ts
+++ src/plugin/entry.ts
@@ -1,11 +1,15 @@
 import { findEntry } from "@finders/find-entry.js";
 import { Config } from "@type/globals.js";
 
+function cleanupWindowsPath(pathValue: string) {
+    return pathValue.replace(/\\/g, "/");
+}
+
 export const generateEntryImport = async (config: Config) => {
     const entryPath = await findEntry(config);
     const defaultEntry = `export const Entry = ({children}) => /*#__PURE__*/createElement(Fragment, null, children);\n`;
     if (!entryPath) {
         return defaultEntry;
     }
-    return `import { Entry as CustomEntry } from '${entryPath}';\nexport const Entry = CustomEntry;\n`;
+    return `import { Entry } from '${cleanupWindowsPath(entryPath)}';\nexport { Entry };\n`;
 };
