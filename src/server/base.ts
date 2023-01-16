05f5ec64f49d05615f2c64eeed8b38e984218786
Snaer <87044139+snowytime@users.noreply.github.com>
2023-01-15 18:40:29 -0700

    🥳 auto patch

--- src/server/base.ts
+++ src/server/base.ts
@@ -43,7 +43,6 @@ export const getBase = async (
                 "react-dom/client",
                 "@snowytime/vibe",
                 "@snowytime/react-hooks",
-                "@ladle/react-context",
                 "react-router-dom",
             ],
             exclude: ["@swc/wasm", "@swc/core", "fsevents", "virtual:vibe"],
