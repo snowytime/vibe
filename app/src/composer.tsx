c6910389786e70e83f2d9e33f6cb71d142d4fce7
Snaer <87044139+snowytime@users.noreply.github.com>
2023-02-20 12:02:04 -0700

    ⚡️ auto patch

--- app/src/composer.tsx
+++ app/src/composer.tsx
@@ -1,22 +1,21 @@
 import React from "react";
 
 export const composer = (module: any, storyName: any, data: any) => {
-	// some initial things
-	const defaultExport = module.default ?? {};
-	const component = module[storyName];
-	const vibe = module[storyName].vibe ?? {};
-	const defaultDecorator =
-		defaultExport.decorator || (({ Component }) => <Component />);
-	const storyDecorator = vibe.decorator || (({ Component }) => <Component />);
-	// bring them together
-	const args = { ...defaultExport, ...vibe, ...data };
-	return () =>
-		defaultDecorator({
-			Component: () =>
-				storyDecorator({
-					Component: () => React.createElement(component, args),
-					...args
-				}),
-			...args
-		});
+    // some initial things
+    const defaultExport = module.default ?? {};
+    const component = module[storyName];
+    const vibe = module[storyName].story ?? {};
+    const defaultDecorator = defaultExport.decorator || (({ Component }) => <Component />);
+    const storyDecorator = vibe.decorator || (({ Component }) => <Component />);
+    // bring them together
+    const args = { ...defaultExport, ...vibe, ...data };
+    return () =>
+        defaultDecorator({
+            Component: () =>
+                storyDecorator({
+                    Component: () => React.createElement(component, args),
+                    ...args,
+                }),
+            ...args,
+        });
 };
