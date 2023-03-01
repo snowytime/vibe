8e8a3e5f32832e62ff0fa2bdce9300d4b2170c7b
Snaer <87044139+snowytime@users.noreply.github.com>
2023-02-28 19:46:45 -0700

    ⚡️ auto patch

--- src/finders/find-stories.ts
+++ src/finders/find-stories.ts
@@ -1,10 +1,12 @@
-import { finderDebugger } from "#debug/index.js";
-import { Config } from "#type/index.js";
 import { globby } from "globby";
 import { join } from "node:path";
+import { finderDebugger } from "#debug/index.js";
+import { Config } from "#type/index.js";
 
 export const findStories = async (config: Config) => {
-    const results = await globby(config.stories);
+    const results = await globby(config.stories, {
+        dot: true,
+    });
     if (results.length) {
         finderDebugger(`Soured ${results.length} stories`);
     }
