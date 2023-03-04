60bb840f4101dbc044d5e1ee68e2a6911394b268
Snaer <87044139+snowytime@users.noreply.github.com>
2023-03-04 16:09:47 -0700

    ⚡️ auto patch

--- src/server/base.ts
+++ src/server/base.ts
@@ -1,4 +1,5 @@
-import { join } from "node:path";
+import { join, dirname } from "node:path";
+import { fileURLToPath } from "node:url";
 import tsconfigPaths from "vite-tsconfig-paths";
 import reactPlugin from "@vitejs/plugin-react";
 import vibePlugin from "../plugin/vibe-plugin.js";
@@ -6,6 +7,9 @@ import { Config } from "#type/globals.js";
 
 export const getBase = async (configUpdate: any, userConfig: Config) => {
     // get the user configuration.
+    const __filename = fileURLToPath(import.meta.url);
+
+    const __dirname = dirname(__filename);
     // note that we NEED the tsconfigPaths and react plugins to be present.
     // the user may supply their own custom vite config that will be applied as well during dev, build, and start phases
     const config = {
