327a1837b555391bef156262bd6e0e41b37e0a74
Snaer <87044139+snowytime@users.noreply.github.com>
2023-03-04 17:14:34 -0700

    ⚡️ auto patch

---
+++ src/app/src/ui/index.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { Category } from "@snowytime/vibe";
+import { CloseIcon, MenuIcon, MoonIcon, SunCirclesIcon } from "@snowytime/iconly-react/ui";
+import "./index.scss";
+import { Tree } from "./tree/index.js";
+
+export const useTheme = () => {
+    const [theme, setTheme] = React.useState("light");
+    const toggleTheme = React.useCallback(() => {
+        if (theme === "light") {
+            setTheme("dark");
+        } else {
+            setTheme("light");
+        }
+    }, [theme]);
+    React.useLayoutEffect(() => {
+        document.documentElement.setAttribute("data-theme", theme);
+        document.documentElement.style.transition = "color 0.2s ease-in-out";
+    }, [theme]);
+    return { theme, setTheme, toggleTheme };
+};
+
+export const Ui = ({
+    children,
+    title,
+    tree,
+}: {
+    children: React.ReactNode;
+    title: string;
+    tree: Category[];
+}) => {
+    const [open, setOpen] = React.useState(true);
+    const { theme, toggleTheme } = useTheme();
+    return (
+        <main className='vibe-wrapper'>
+            <nav className='vibe-nav'>
+                <div className='vibe-toolbar'>
+                    <div className='sidebar-btn' onClick={() => setOpen((s) => !s)}>
+                        {open ? <CloseIcon height='40%' /> : <MenuIcon height='50%' />}
+                    </div>
+                    <div className='sidebar-btn' onClick={toggleTheme}>
+                        {theme === "light" ? (
+                            <SunCirclesIcon height='55%' />
+                        ) : (
+                            <MoonIcon height='45%' />
+                        )}
+                    </div>
+                </div>
+                <div className='vibe-title'>{title}</div>
+            </nav>
+            <section className='vibe-content'>
+                <div
+                    style={{
+                        width: open ? "" : "0",
+                        transition: "width 0.2s ease-in-out",
+                        border: open ? "" : "none",
+                    }}
+                    className='vibe-content-sidebar'
+                >
+                    <Tree data={tree} />
+                </div>
+                <div className='vibe-content-window'>{children}</div>
+            </section>
+        </main>
+    );
+};
---
+++ src/app/src/ui/tree/index.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { Category } from "@snowytime/vibe";
+import { useVibeContext } from "@snowytime/vibe/client";
+import {
+    BookIcon,
+    BookmarkIcon,
+    FolderIcon,
+    MinusSquareIcon,
+    PlusSquareIcon,
+} from "@snowytime/iconly-react/ui";
+
+import "./index.scss";
+
+const Nest = ({ category }: { category: Category }) => {
+    const { useNavigate, useLocation } = useVibeContext();
+    const navigate = useNavigate();
+    const location = useLocation();
+    return category.type === "file" ? (
+        <div
+            data-toggled={location.pathname === category.path}
+            className='header'
+            onClick={() => navigate(category.path || "/")}
+        >
+            <div style={{ height: "10px", width: "10px", marginRight: "5px" }} />
+            <div>
+                <BookmarkIcon />
+            </div>
+            {category.name}
+        </div>
+    ) : (
+        <Folder category={category} />
+    );
+};
+
+export const Tree = ({ data }: { data: Category[] }) => {
+    return (
+        <div>
+            {data.map((category) => (
+                <Nest category={category} />
+            ))}
+        </div>
+    );
+};
+
+const Folder = ({ category }: { category: Category }) => {
+    const [open, setOpen] = React.useState(false);
+    return (
+        <div>
+            <div className='header' onClick={() => setOpen((o) => !o)}>
+                <div style={{ height: "10px", width: "10px", marginRight: "5px" }}>
+                    {open ? <MinusSquareIcon /> : <PlusSquareIcon />}
+                </div>
+                <div>
+                    {category.children.some((i) => i.type === "file") ? (
+                        <BookIcon />
+                    ) : (
+                        <FolderIcon />
+                    )}
+                </div>
+                {category.name}
+            </div>
+            {open && (
+                <div style={{ marginLeft: "14px", borderLeft: "1px solid hsl(var(--accent-100))" }}>
+                    {category.children.map((child) => (
+                        <Nest category={child} />
+                    ))}
+                </div>
+            )}
+        </div>
+    );
+};
