219bc5bc635ea0daa90c7421bd77c3d752f8b2ba
Snaer <87044139+snowytime@users.noreply.github.com>
2023-03-04 17:42:57 -0700

    ⚡️ auto patch

--- src/app/src/main.tsx
+++ src/app/src/main.tsx
@@ -14,56 +14,57 @@ import { Context } from "./context.js";
 const Main = () => {
     const memoized = React.useMemo(() => ({ stories, storyTree, config, storyUrls }), []);
     return (
-        <BrowserRouter>
-            <Context.Provider value={memoized}>
-                <Entry>
-                    <Routes>
-                        <Route
-                            path='/'
-                            element={
-                                <Navigate
-                                    replace
-                                    to={
-                                        config.defaultStory
-                                            ? config.defaultStory
-                                            : Object.values(stories)[0].url
-                                    }
-                                />
-                            }
-                        />
-                        {Object.values(stories).map((story) => {
-                            return (
-                                <Route
-                                    key={story.id}
-                                    path={story.url}
-                                    element={
-                                        <React.Suspense fallback={<>loading...</>}>
-                                            <Ui title='css-stuff' tree={storyTree}>
+        <Ui title='css-stuff' tree={storyTree}>
+            <BrowserRouter>
+                <Context.Provider value={memoized}>
+                    <Entry>
+                        <Routes>
+                            <Route
+                                path='/'
+                                element={
+                                    <Navigate
+                                        replace
+                                        to={
+                                            config.defaultStory
+                                                ? config.defaultStory
+                                                : Object.values(stories)[0].url
+                                        }
+                                    />
+                                }
+                            />
+                            {Object.values(stories).map((story) => {
+                                return (
+                                    <Route
+                                        key={story.id}
+                                        path={story.url}
+                                        element={
+                                            <React.Suspense fallback={<>loading...</>}>
                                                 {React.createElement(story.component)}
-                                            </Ui>
-                                            {React.createElement(story.component)}
-                                        </React.Suspense>
-                                    }
-                                />
-                            );
-                        })}
-                        <Route
-                            path='*'
-                            element={
-                                <Navigate
-                                    replace
-                                    to={
-                                        config.defaultStory
-                                            ? config.defaultStory
-                                            : Object.values(stories)[0].url
-                                    }
-                                />
-                            }
-                        />
-                    </Routes>
-                </Entry>
-            </Context.Provider>
-        </BrowserRouter>
+
+                                                {React.createElement(story.component)}
+                                            </React.Suspense>
+                                        }
+                                    />
+                                );
+                            })}
+                            <Route
+                                path='*'
+                                element={
+                                    <Navigate
+                                        replace
+                                        to={
+                                            config.defaultStory
+                                                ? config.defaultStory
+                                                : Object.values(stories)[0].url
+                                        }
+                                    />
+                                }
+                            />
+                        </Routes>
+                    </Entry>
+                </Context.Provider>
+            </BrowserRouter>
+        </Ui>
     );
 };
 
