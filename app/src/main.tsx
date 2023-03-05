9e2affe20a0e8e080aa0ade2139b714a16840cff
Snaer <87044139+snowytime@users.noreply.github.com>
2023-03-04 21:03:15 -0700

    ⚡️ auto patch

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -4,7 +4,6 @@ import { stories, storyTree, storyUrls, config, Entry } from "virtual:vibe";
 
 import { Ui } from "../src/ui/index.js";
 
-import "../vibe.scss";
 import "@snowytime/css/fonts/visby/all.css";
 import "@snowytime/css/presets/visby.css";
 
